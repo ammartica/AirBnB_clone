3baa56f0271dd8b630b82d64ceb5aa5a3f4fb57a
ammartica <amisadaymartinez@gmail.com>
2022-03-01 23:34:06 +0000

    added storage method calls

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """print [<class name>] (<self.id>) <self.__dict__>"""
@@ -30,6 +32,7 @@ class BaseModel:
     def save(self):
         """update instance attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dict containing values of self.__dict__"""
