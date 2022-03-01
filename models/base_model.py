729b4c94bc82949965b0b65b2ad9481ef4343b9e
ammartica <amisadaymartinez@gmail.com>
2022-03-01 20:48:29 +0000

    passed test task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes BaseModel"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """print [<class name>] (<self.id>) <self.__dict__>"""
@@ -28,10 +36,7 @@ class BaseModel:
         self_attr = self.__dict__.copy()
         self_attr["__class__"] = self.__class__.__name__
 
-        iso_created_at = self.created_at.isoformat("T", "auto")
-        iso_updated_at = self.updated_at.isoformat("T", "auto")
-
-        self_attr["created_at"] = iso_created_at
-        self_attr["updated_at"] = iso_updated_at
+        self_attr["created_at"] = self.created_at.isoformat("T", "auto")
+        self_attr["updated_at"] = self.updated_at.isoformat("T", "auto")
 
         return self_attr
