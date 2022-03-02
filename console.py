71da79f07fb6190ae34e125215023567d98373a2
Luijma <rsluismanuel@gmail.com>
2022-03-02 00:51:49 +0000

    Adding console: Worked on basis for Create Class, haven't added actual functionality to create the object as I need to first get the commits from amy's branch. Task 7

--- console.py
+++ console.py
@@ -3,22 +3,41 @@
 
 
 import cmd
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """ Overrides methods to personalize command interpreter """
 
     prompt = "(hbnb) "
+
     """ HBNB Commands """
+
+    def do_create(self, arg):
+        """Creates a new instance of class provided as argument """
+        args = arg.split()
+        if len(args) != 1:
+            print("** class name missing **")
+        else:
+            try:
+                class_name = getattr(models, classname)
+                obj = class_name()
+            except NameError:
+                print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """ prints string representation of an instance from
+        class name and id """
+        pass
 
     """ Basic Commands """
 
     def do_quit(self, arg):
-        """ Quit command to exit the program """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """ Quit command to exit the program """
+        """Quit command to exit the program"""
         return True
 
     """ Overwritten methods """
