06ef134cdce6483108482cb2dc6e63194ab38865
Luijma <rsluismanuel@gmail.com>
2022-03-04 22:11:02 +0000

    Broken file_storage code, working on fixing deserialization into __objects. Circular import issue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
 
 import json
 
-
 class FileStorage:
     """Class serializes/deserializes instances to JSON file and back"""
     __file_path = "file.json"
@@ -11,19 +10,21 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file in a nested dict"""
         nested_objs = {}
-        for key, value in FileStorage.__objects.items():
-            nested_objs[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+        for item in self.__objects:
+            nested_objs[item] = {}
+            for key, value in self.__objects[item].to_dict().items():
+                nested_objs[item][key] = value
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
             json.dump(nested_objs, f)
 
 #this function is likely wrong because I am very confused
@@ -31,9 +32,11 @@ class FileStorage:
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
                 d_objects = json.load(f)
-            for key, value in d_objects.items():
-                FileStorage.__objects[key] = value
-        except:
+                for key in d_objects:
+                    class_name = 
+                    new_obj = class_name(d_objects)
+                    self.new(new_obj)
+        except FileNotFoundError:
             pass
