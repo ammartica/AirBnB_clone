cb51b8e644951267e9ffda505c3cedb70c1a9fac
Luijma <rsluismanuel@gmail.com>
2022-03-05 05:20:44 +0000

    There is a bug where the user ids for all the objects that are being reloaded are always different. Need to check if this is an issue with the BaseModel __init__ or with the reload function

--- console.py
+++ console.py
@@ -4,12 +4,14 @@
 
 import cmd
 import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Overrides methods to personalize command interpreter """
 
     prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
 
     """--- HBNB Commands ---"""
 
@@ -19,19 +21,36 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         if len(args) != 1:
             print(HBNBCommand.class_missing_err)
-        else:
+        if len(args) == 1:
             try:
-                class_name = getattr(models, classname)
-                obj = class_name()
-            except NameError:
+                class_name = HBNBCommand.classes[args[0]]
+                new_obj = class_name()
+                print(new_obj.id)
+                models.storage.save()
+            except KeyError:
                 print(HBNBCommand.class_ntexist_err)
+        else:
+            print("Usage: create <class_name>")
 
-        def do_show(self, arg):
-            """ prints string representation of an instance from
-            class name and id """
-            args = arg.split()
+    def do_show(self, arg):
+        """ prints string representation of an instance from
+        class name and id """
+        args = arg.split()
+        if len(args) == 2:
+            obj_key = HBNBCommand.form_key(args[0], args[1])
+            if args[0] not in HBNBCommand.classes:
+                print(HBNBCommand.class_ntexist_err)
+            elif obj_key not in models.storage.all():
+                print(HBNBCommand.no_instance_found_err)
+            else:
+                print(models.storage.all()[obj_key])
+        else:
+            if len(args) == 0:
+                print(HBNBCommand.class_missing_err)
+            elif len(args) == 1:
+                print(HBNBCommand.instance_id_missing_err)
 
-    """ ---Basic Commands--- """
+    # ---Basic Commands---
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -41,18 +60,32 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         return True
 
-    """---Overwritten methods---"""
+    # ---Overwritten methods---
 
     def emptyline(self):
         pass
 
-    """---Helper methods---"""
+    # ---Helper methods---
 
-    """---ERROR MESSAGES---"""
+    def form_key(classname, uuid):
+        return classname + "." + uuid
+
+    def do_printkey(self, arg):
+        args = arg.split()
+        print(HBNBCommand.form_key(args[0], args[1]))
+
+    def do_showall(self, arg):
+        print(models.storage.all())
+
+    # ---ERROR MESSAGES---
 
     class_missing_err = "** class name missing **"
     class_ntexist_err = "** class doesn't exist **"
     class_no_id_err = "** instance id missing **"
+    no_instance_found_err = " ** no instance found **"
+    instance_id_missing_err = " ** instance id missing **"
+    attr_name_missing_err = "** attribute name missing **"
+    value_missing_err = " ** value missing **"
 
 
 if __name__ == '__main__':
