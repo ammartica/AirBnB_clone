aff2fd29520d749d35ae0ae03b70d10f644f179e
Luijma <rsluismanuel@gmail.com>
2022-03-06 06:24:40 +0000

    Finished Update Function. Code should work with all commands. Tomorrow is debug and unittest day

--- console.py
+++ console.py
@@ -57,7 +57,7 @@ class HBNBCommand(cmd.Cmd):
         if (error == HBNBCommand.class_ntexist_err):
             print(error)
         if (error == HBNBCommand.class_missing_err or
-                error == None):
+                error is None):
             # If the only error is class missing
             # that means no args were given
             # So code can continue
@@ -91,9 +91,21 @@ class HBNBCommand(cmd.Cmd):
             models.storage.save()
 
     def do_update(self, arg):
+        """ Updates instance based on class_name, id, attribute and value"""
         args = arg.split()
-
-
+        error = HBNBCommand.find_error(args, 4)
+        if error is None:
+            # fetches object through id, then checks attr_type
+            # if attr_type is a simple type it'll set the attr
+            # using args[4], and converts it
+            simple_types = {"int": int, "str": str, "float": float}
+            obj_key = HBNBCommand.form_key(args[0], args[1])
+            obj = models.storage.all()[obj_key]
+            attr_type = type(getattr(obj, args[2]))
+            setattr(models.storage.all()[obj_key], args[2], attr_type(args[3]))
+            models.storage.save()
+        else:
+            print(error)
 
     # ---Basic Commands---
 
@@ -124,7 +136,7 @@ class HBNBCommand(cmd.Cmd):
                 return HBNBCommand.class_missing_err
 
             elif len(args) == 1:
-                if HBNBCommand.class_exists(args[0]) == False:
+                if HBNBCommand.class_exists(args[0]) is False:
                     # if only one arg, check if class exists
 
                     return HBNBCommand.class_ntexist_err
@@ -134,37 +146,46 @@ class HBNBCommand(cmd.Cmd):
 
                     return HBNBCommand.instance_id_missing_err
 
-            elif len(args) == 2 and HBNBCommand.class_exists(args[0]):
-                # if 2 args and class exists
-                # check if id is valid
+            elif len(args) == 2:
+                # if at least 2 args check if class exists
+                # then check if id is valid
+                if (HBNBCommand.class_exists(args[0]) is False):
 
-                if instance_exists(obj_key) == False:
+                    return HBNBCommand.class_ntexist_err
+
+                if (HBNBCommand.instance_exists(obj_key) is is False):
+
                     return HBNBCommand.no_instance_found_err
                 else:
-                # if id is valid, we're missing attribute name
+                    # if id is valid, we're missing attribute name
 
                     return HBNBCommand.attr_name_missing_err
 
-            elif (len(args) == 3 and HBNBCommand.class_exists(args[0]) and
-                    HBNBCommand.instance_exists(obj_key)):
+            elif (len(args) == 3):
+                # if 3 args, check if class and attr_id exists
+                if (HBNBCommand.class_exists(args[0]) is False):
+                    return HBNBCommand.class_ntexist_err
+
+                if (HBNBCommand.instance_exists(obj_key) is False):
+                    return HBNBCommand.no_instance_found_err
+
                 # if only 3 arguments, attr value is missing
 
                 return HBNBCommand.value_missing_err
 
         elif len(args) >= expected:
-            if HBNBCommand.class_exists(args[0]) == False:
+            if HBNBCommand.class_exists(args[0]) is False:
                 # correct number of args,
                 # so check if class exists
                 return HBNBCommand.class_ntexist_err
             elif (expected >= 2 and
-                HBNBCommand.instance_exists(obj_key) == False):
+                  HBNBCommand.instance_exists(obj_key) is False):
                 # If we have an id but id is invalid
                 # Then return an error
-                    return HBNBCommand.no_instance_found_err
+                return HBNBCommand.no_instance_found_err
             else:
                 # If no errors found, return no error
                 return None
-
 
     def class_exists(class_name):
         if class_name not in HBNBCommand.classes:
@@ -177,13 +198,6 @@ class HBNBCommand(cmd.Cmd):
             return False
         else:
             return True
-    
-    def do_printkey(self, arg):
-        args = arg.split()
-        print(HBNBCommand.form_key(args[0], args[1]))
-
-    def do_showall(self, arg):
-        print(models.storage.all())
 
     # ---ERROR MESSAGES---
 
