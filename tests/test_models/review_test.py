05584cec20bfd8ec66536fbc252b28d290beb0ac
Luijma <rsluismanuel@gmail.com>
2022-03-06 21:09:32 +0000

    Adding the emptier tests suits

---
+++ tests/test_models/review_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" review class Test Suite """
+
+
+import unittest
+# from models.review_test.py import Review
+
+# class Test_Review(unittest.TestCase):
+#    def setup():
+    
+if __name__ == '__main__':
+    unittest.main()
