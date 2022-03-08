4652767bd0d3750d53289697f23aa19343850130
Luijma <rsluismanuel@gmail.com>
2022-03-08 16:52:04 +0000

    Adding engine unit test base

---
+++ tests/test_engine/test_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" city class Test Suite """
+
+
+import unittest
+from models.engine.file_storage import file_storage.py
+
+
+class Test_City(unittest.TestCase):
+    def setup():
+        """ sets up city tests """
+        pass
+    def test_save():
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
